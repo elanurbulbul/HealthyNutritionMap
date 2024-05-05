8f8e9292a108c221ec690a4e702e71715ec972aa
Elanur Bulbul <126582672+elanurbulbul@users.noreply.github.com>
2024-05-05 10:35:12 +0300

    SagliklimijsEdited

--- src/pages/ProductsPage/SaglikliMi.js
+++ src/pages/ProductsPage/SaglikliMi.js
@@ -1,131 +1,95 @@
-import React, { useEffect, useState, useRef } from "react";
-import * as onnx from "onnxjs";
-import Loading from "../../components/Loading";
-const onnxFolder = require("../../model/best.onnx");
+import React, { useRef, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+import Webcam from "react-webcam";
+import * as cocossd from "@tensorflow-models/coco-ssd";
+import drawRect from "./utilities";
 
+// https://github.com/nicknochnack/ReactComputerVisionTemplate/tree/main
 
 function SaglikliMi() {
-  const [loading, setLoading] = useState(false);
-  const [model, setModel] = useState(null);
-  const [predictions, setPredictions] = useState([]);
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
 
-  // ONNX modelini yükleme
-  
+  // Main function
+  const runCoco = async () => {
+    const net = await cocossd.load();
+    setInterval(() => {
+      detect(net);
+    }, 10);
+  };
+
+  const detect = async (net) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // 4. TODO - Make Detections
+      // e.g. const obj = await net.detect(video);
+      const obj = await net.detect(video);
+      //console.log("Obj:", obj);
+
+      // Draw mesh
+      const ctx = canvasRef.current.getContext("2d");
+
+      drawRect(obj, ctx);
+    } else {
+      console.log("No web");
+    }
+  };
 
   useEffect(() => {
-    async function loadModel() {
-      //const modelPath = onnxFolder;
-      console.log("infer2");
-      const sess = await new onnx.InferenceSession();
-      console.log("startin");
-      const loadedModel = await sess.loadModel("../../model/best.onnx");
-      console.log("sess load");
-      setModel(loadedModel);
-    }
-     loadModel();
+    runCoco();
   }, []);
-
-  // Görüntü işleme ve tahmin yapma
-  async function handleImageProcessing(image) {
-    if (!model) return;
-
-    // Görüntüyü tensor formatına dönüştürme
-    const tensor = new onnx.Tensor(new Float32Array(image.data), "float32", [
-      1,
-      image.height,
-      image.width,
-      3,
-    ]);
-
-    // Modeli kullanarak tahmin yapma
-    const output = await model.predict(tensor);
-    setPredictions(output);
-  }
-
-  // Kullanıcının bir dosya yüklediğinde tetiklenecek işlev
-  function handleFileUpload(event) {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-    reader.onload = function (event) {
-      const image = new Image();
-      image.onload = function () {
-        handleImageProcessing(image);
-      };
-      image.src = event.target.result;
-    };
-    reader.readAsDataURL(file);
-  }
-
-  const useWebcam = () => {
-    const [videoStream, setVideoStream] = useState(null);
-    const canvasRef = useRef(null);
-
-    useEffect(() => {
-      async function startWebcam() {
-        const constraints = { video: true };
-        const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        setVideoStream(stream);
-      }
-
-      startWebcam();
-
-      return () => {
-        if (videoStream) {
-          videoStream.getTracks().forEach((track) => track.stop());
-        }
-      };
-    }, []);
-
-    useEffect(() => {
-      if (videoStream && canvasRef) {
-        const videoElement = document.createElement("video");
-        videoElement.srcObject = videoStream;
-        videoElement.play();
-
-        const context = canvasRef.current.getContext("2d"); // Use current here
-        const interval = setInterval(() => {
-          context.drawImage(videoElement, 0, 0);
-          const imageData = context.getImageData(
-            0,
-            0,
-            canvasRef.current.width,
-            canvasRef.current.height
-          ); // Use current here
-          handleImageProcessing(imageData);
-        }, 1000);
-
-        return () => clearInterval(interval);
-      }
-    }, [videoStream, canvasRef]);
-
-    return (
-      <div>
-        <video
-          ref={(videoElement) =>
-            videoElement && (videoElement.srcObject = videoStream)
-          }
-          autoPlay
-          muted
-        ></video>
-        <canvas ref={canvasRef}></canvas>
-      </div>
-    );
-  };
 
   return (
     <div>
-      {loading && <Loading />}
-      <h1>Nesne Tahmini Web Sitesi</h1>
-      <input type="file" accept="image/*" onChange={handleFileUpload} />
-      {useWebcam()}
-      <div>
-        <h2>Tahminler</h2>
-        <ul>
-          {predictions.map((prediction, index) => (
-            <li key={index}>{prediction}</li>
-          ))}
-        </ul>
-      </div>
+      <header>
+        <Webcam
+          ref={webcamRef}
+          muted={true}
+          style={{
+           // position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 8,
+            width: 640,
+            height: 480,
+          }}
+        />
+      </header>
     </div>
   );
 }
---
+++ src/pages/ProductsPage/_SaglikliMi.js
@@ -0,0 +1,131 @@
+import React, { useEffect, useState, useRef } from "react";
+import * as onnx from "onnxjs";
+import Loading from "../../components/Loading";
+const onnxFolder = require("../../model/best.onnx");
+
+function SaglikliMi() {
+  const [loading, setLoading] = useState(false);
+  const [model, setModel] = useState(null);
+  const [predictions, setPredictions] = useState([]);
+
+  // ONNX modelini yükleme
+
+  useEffect(() => {
+    async function loadModel() {
+      //const modelPath = onnxFolder;
+      // console.log("infer2");
+      // const sess = await new onnx.InferenceSession();
+      // console.log("startin");
+      // const loadedModel = await sess.loadModel("../../model/best.onnx");
+      // console.log("sess load");
+      // setModel(loadedModel);
+    }
+    loadModel();
+  }, []);
+
+  // Görüntü işleme ve tahmin yapma
+  async function handleImageProcessing(image) {
+    if (!model) return;
+
+    // Görüntüyü tensor formatına dönüştürme
+    const tensor = new onnx.Tensor(new Float32Array(image.data), "float32", [
+      1,
+      image.height,
+      image.width,
+      3,
+    ]);
+
+    // Modeli kullanarak tahmin yapma
+    const output = await model.predict(tensor);
+    setPredictions(output);
+  }
+
+  // Kullanıcının bir dosya yüklediğinde tetiklenecek işlev
+  function handleFileUpload(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = function (event) {
+      const image = new Image();
+      image.onload = function () {
+        handleImageProcessing(image);
+      };
+      image.src = event.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  const useWebcam = () => {
+    const [videoStream, setVideoStream] = useState(null);
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+      async function startWebcam() {
+        const constraints = { video: true };
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        setVideoStream(stream);
+      }
+
+      startWebcam();
+
+      return () => {
+        if (videoStream) {
+          videoStream.getTracks().forEach((track) => track.stop());
+        }
+      };
+    }, [videoStream]);
+
+    useEffect(() => {
+      if (videoStream && canvasRef) {
+        const videoElement = document.createElement("video");
+        videoElement.srcObject = videoStream;
+        videoElement.play();
+
+        const context = canvasRef.current.getContext("2d"); // Use current here
+        const interval = setInterval(() => {
+          context.drawImage(videoElement, 0, 0);
+          const imageData = context.getImageData(
+            0,
+            0,
+            canvasRef.current.width,
+            canvasRef.current.height,
+          ); // Use current here
+          handleImageProcessing(imageData);
+        }, 1000);
+
+        return () => clearInterval(interval);
+      }
+    }, [videoStream, canvasRef]);
+
+    return (
+      <div>
+        <video
+          ref={(videoElement) =>
+            videoElement && (videoElement.srcObject = videoStream)
+          }
+          autoPlay
+          muted
+        ></video>
+        <canvas ref={canvasRef}></canvas>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      {loading && <Loading />}
+      <h1>Nesne Tahmini Web Sitesi</h1>
+      <input type="file" accept="image/*" onChange={handleFileUpload} />
+      {useWebcam()}
+      <div>
+        <h2>Tahminler</h2>
+        <ul>
+          {predictions.map((prediction, index) => (
+            <li key={index}>{prediction}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default SaglikliMi;
