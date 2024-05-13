39ae79c6542885f1e562c3e404bd879309802345
Elanur Bülbül <elanrbulbul@gmail.com>
2024-05-13 14:47:05 +0300

    EditedHeader

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,6 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { pages } from './data';
-import cx from 'classnames';
 import './index.scss';
 
 
@@ -12,14 +11,14 @@ function BasicExample() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand className={cx('navbar-brand')} href="/anasayfa">Sağlıklı Besin Haritası</Navbar.Brand>
+        <Navbar.Brand className='navbar-brand' href="/homepage">Healthy Nutrition Map</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav " className='justify-content-end '>
           
-          <Nav className={cx('navbar-list')}>
+          <Nav className='navbar-list'>
                {pages.map((page) => (
                  <Nav.Link href={page.link} key={page.id}
-                 className={cx('navbar-list-item')}
+                 className='navbar-list-item'
                 
                  >
                    {page.text}
